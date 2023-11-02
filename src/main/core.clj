476ad1741744e495c22e7597bc12ddf6432b523a
Flash Hu <idisblueflash@gmail.com>
2023-11-02 20:32:44 +0800

    core search function

--- src/main/core.clj
+++ src/main/core.clj
@@ -1,11 +1,27 @@
-(ns core)
+(ns core
+  (:require [clojure.core.matrix :as m]))
+
+(m/set-current-implementation :vectorz)
 
 (defn fetch-all-contents [])
 
-(defn embedding-text [text]
-  )
+(defn embedding-text [text])
+
+(defn calculate-similarity [embedding-a embedding-b]
+  (m/dot embedding-a embedding-b))
+
+(defn convert-and-round [num]
+  (->> num
+       (* 100)
+       (format "%.2f")
+       (Double/parseDouble)))
 
 (defn search [question]
-  [{:id 1 :text "foo"}
-   {:id 2 :text "bar"}
-   {:id 3 :text "baz"}])
+  (let [question-embedding (embedding-text question)]
+    (->> (fetch-all-contents)
+         (map #(assoc % :similarity
+                        (calculate-similarity question-embedding (:embedding %))))
+         (sort-by (comp - :similarity))
+         (map #(select-keys % [:id :text :similarity] ))
+         (map #(assoc % :similarity (convert-and-round (:similarity %))))
+         (take 3))))
--- src/test/unit_tests/search_test.clj
+++ src/test/unit_tests/search_test.clj
@@ -1,27 +1,40 @@
 (ns unit-tests.search-test
   (:require [clojure.test :refer [deftest is testing]]
+            [clojure.string :as str]
             [core :refer [search]]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+
+            [core :as stub-core]))
 
 (def fixtures
-  [{:id 1 :type :db :text "The sun sets over the tranquil city, painting the sky in hues of orange and pink."}
-   {:id 2 :type :db :text "A symphony of birdsong fills the air as dawn breaks over the forest."}
-   {:id 3 :type :db :text "The bustling marketplace is alive with vibrant colors, fragrant spices, and animated conversations."}
-   {:id 4 :type :db :text "Beneath the shimmering surface of the lake, fish dart to and fro, their scales reflecting the sunlight."}
-   {:id 5 :type :db :text "In the heart of the metropolis, skyscrapers touch the clouds and streets pulse with energy."}
-   {:id 6 :type :db :text "The ancient temple, worn by time, stands as a testament to civilizations long forgotten."}
-   {:id 7 :type :db :text "Laughter echoes through the park as children fly kites and play games under the watchful eyes of towering trees."}
-   {:id 8 :type :db :text "On the mountaintop, the world stretches out below, a vast tapestry of landscapes and horizons."}
-   {:id 9 :type :db :text "The artist's brush dances across the canvas, capturing the essence of a moment in bold strokes and vivid colors."}])
+  (-> "src/test/unit_tests/fixtures.edn" (slurp) (edn/read-string)))
 
 (deftest search-test
-  (testing "search for underwater life"
-    (is (= (search "Which text mentions a setting where there's a focus on the underwater life of a lake?")
-           [{:id 4 :text "Beneath the shimmering surface of the lake, fish dart to and fro, their scales reflecting the sunlight."}
-            {:id 2 :text "bar"}
-            {:id 3 :text "baz"}]))))
+  (with-redefs [stub-core/fetch-all-contents (fn [] (->> fixtures (filter #(= (:type %) :db))))
+                stub-core/embedding-text (fn [text] (->> fixtures
+                                                         (filter #(= (:type %) :question))
+                                                         (filter #(str/starts-with? (:text %) text))
+                                                         first
+                                                         :embedding))]
+    (testing "search for underwater life"
+      (is (= (search "Which text mentions a setting where there's a focus on the underwater life of a lake?")
+             [{:id 4 :similarity 84.82 :text "Beneath the shimmering surface of the lake, fish dart to and fro, their scales reflecting the sunlight."}
+              {:id 8 :similarity 78.4 :text "On the mountaintop, the world stretches out below, a vast tapestry of landscapes and horizons."}
+              {:id 1 :similarity 77.59 :text "The sun sets over the tranquil city, painting the sky in hues of orange and pink."}])))
+    (testing "search for building and clouds"
+      (is (= (search "In which sample is there a description of a city's tall buildings touching the clouds?")
+             [{:id 5 :similarity 85.43 :text "In the heart of the metropolis, skyscrapers touch the clouds and streets pulse with energy."}
+              {:id 1 :similarity 81.73 :text "The sun sets over the tranquil city, painting the sky in hues of orange and pink."}
+              {:id 8 :similarity 79.9 :text "On the mountaintop, the world stretches out below, a vast tapestry of landscapes and horizons."}])))
+    (testing "search for break day and birds singing"
+      (is (= (search "Which embedded text captures a scene at the break of day with birds singing?")
+             [{:id 2 :similarity 88.79 :text "A symphony of birdsong fills the air as dawn breaks over the forest."}
+              {:id 1 :similarity 82.28 :text "The sun sets over the tranquil city, painting the sky in hues of orange and pink."}
+              {:id 9 :similarity 80.9 :text "The artist's brush dances across the canvas, capturing the essence of a moment in bold strokes and vivid colors."}]))))
 
-
+  )
 (comment
-  (-> "src/test/unit_tests/fixtures.edn" (slurp) (edn/read-string))
-  ,)
+  (->> fixtures
+       (filter #(= (:type %) :question))
+       (map #(select-keys % [:id :text]))
+       ),)
